d7e734871e31c07aec0e8b5d2dce1a554d7dd5a1
Archakov06 <hello@archakov.im>
2017-06-20 14:30:37 +0300

    updated - 20.051.2017 - 14:30

--- src/index.js
+++ src/index.js
@@ -15,11 +15,14 @@ parsers.forEach((p) => p.start());
 
 setTimeout(() => {
   exec('git add .');
+  console.log('GIT ADD');
     setTimeout(() => {
-      const date = `updated - ${new Date().getDate()}.${new Date().getMonth()+1}.${new Date().getFullYear()} - ${new Date().getHours()}:${new Date().getMinutes()}`;
+      const date = `updated - ${new Date().getDate()}.${new Date().getMonth()+1 < 10 ? '0' + new Date().getMonth()+1 : new Date().getMonth()+1}.${new Date().getFullYear()} - ${new Date().getHours()}:${new Date().getMinutes()}`;
       exec(`git commit -m "${date}"`);
+      console.log('GIT COMMIT - ' + date);
         setTimeout(() => {
           exec('git push origin master');
+          console.log('GIT PUSH');
         }, 1000);
     }, 1000);
 }, 10000);
